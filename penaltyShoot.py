3db88651d9db08339b0592ec30d5d4527aaae747
Khathawut Chaipraphun <bomb_khathawut@hotmail.com>
2016-12-22 18:02:36 +0700

    add random shoot target

--- penaltyShoot.py
+++ penaltyShoot.py
@@ -1,4 +1,6 @@
 import arcade
+from random import randint, random
+from random import shuffle, random
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -11,8 +13,13 @@ MARGIN = 3;
 class SpaceGameWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
-
+        self.timeStart = 0.0
+        self.temp = 0.0
+        self.temp2 = 0.0
+        self.SpacePress = False
         self.grid = []
+        self.dataRow = [6,8,10]
+        self.dataCol = [1,4,7,10,13]
         for row in range(14):
             # Add an empty array that will hold each cell
             # in this row
@@ -21,12 +28,28 @@ class SpaceGameWindow(arcade.Window):
                 self.grid[row].append(0)  # Append a cell
 
 
-        arcade.set_background_color(arcade.color.BLACK)
+        arcade.set_background_color(arcade.color.WHITE)
 
         # self.ship = arcade.Sprite('ship.png', 5)
         # self.ship.set_position(100, 100)
-        self.bg = arcade.Sprite('bg.png')
+        self.bg = arcade.Sprite('bg.jpg')
         self.bg.set_position(400,300)
+
+    def animate(self, delta_time):
+        """
+        All the logic to move, and the game logic goes here.
+        """
+        if(self.SpacePress == True):
+            if(self.timeStart < self.temp + 4):
+                shuffle(self.dataCol)
+                shuffle(self.dataRow)
+                self.grid[self.dataRow[0]][self.dataCol[0]] = 1
+                self.grid[self.dataRow[1]][self.dataCol[1]] = 0
+            else :
+                self.SpacePress = False
+
+
+        self.timeStart += delta_time
 
     def checkPosition(self,row,column):
         check = 0
@@ -54,8 +77,9 @@ class SpaceGameWindow(arcade.Window):
         for row in range(14):
             for column in range(20):
                 if self.checkPosition(row,column):
+                    #self.randomPosition()
                     if self.grid[row][column] == 1:
-                        color = arcade.color.GREEN
+                        color = arcade.color.BLACK
                     else:
                         color = arcade.color.WHITE
 
@@ -66,7 +90,27 @@ class SpaceGameWindow(arcade.Window):
                     # Draw the box
                     arcade.draw_rectangle_filled(x, y, WIDTH, HEIGHT, color)
 
+    def on_key_press(self, key, modifiers):
+        """
+        Called whenever the mouse moves.
+        """
+        if key == arcade.key.SPACE:
+            self.temp = self.timeStart
+            self.temp2 = self.timeStart
+            self.SpacePress = True
+            # row = randint(6,10)
+            # column = randint(1,13)
+            # self.grid[row][column] = 1
 
+
+    # def randomPosition(self,delta):
+    #     self.totalTime = 0.0
+    #     if(self.totalTime < 3):
+    #         row = randint(6,10)
+    #         column = 1
+    #         self.grid[row][column] = 1
+    #
+    #     self.totalTime += delta
 
     def on_mouse_press(self, x, y, button, modifiers):
         """
@@ -81,7 +125,6 @@ class SpaceGameWindow(arcade.Window):
         self.grid[row][column] = 1
         print("Click coordinates: ({}, {}). Grid coordinates: ({}, {})"
               .format(x, y, row, column))
-
 
 
 if __name__ == '__main__':
